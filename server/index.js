afcf43fff45954aaea5778bef903a09c846d0869
Sichelle Chiffert <sichelle.chiffert@gmail.com>
2018-04-04 17:15:12 +0200

    Load mocks

--- server/index.js
+++ server/index.js
@@ -1,5 +1,15 @@
 const express = require('express')
 
+const alumni1 = require ('../mocks/alumnis/alumni1.json')
+const alumni2 = require ('../mocks/alumnis/alumni2.json')
+const alumni3 = require ('../mocks/alumnis/alumni3.json')
+const alumni4 = require ('../mocks/alumnis/alumni4.json')
+const alumni5 = require ('../mocks/alumnis/alumni5.json')
+
+const alumnis = [ alumni1 , alumni2 , alumni3, alumni4, alumni5 ]
+
+console.log(alumnis)
+
 const app = express()
 
 app.get('/', (request,response) => {
